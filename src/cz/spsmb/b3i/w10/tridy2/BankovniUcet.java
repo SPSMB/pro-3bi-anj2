38f70b60c9b31773b7e8b8aff9582e0a056fc0be
jakub.chomanic <jakub.chomanic@student.spsmb.cz>
2021-11-11 14:45:26 +0100

    getCelkovaHotovost

--- src/cz/spsmb/b3i/w10/tridy2/BankovniUcet.java
+++ src/cz/spsmb/b3i/w10/tridy2/BankovniUcet.java
@@ -8,29 +8,39 @@ public class BankovniUcet {
     private int klientskeCislo;
 
     public BankovniUcet(int zustatek, int klientskeCislo) {
-        //zde nepoužívat this. Sice to možné je, ale vzhledem k tomu, že jde o proměnnou třídy
+        //zde nepouzivat this. Sice je to možné, ale vzhledem k tomu, že proměnnou třídy
         //bylo by to nepřehledné
-        BankovniUcet.aktualniCisloUctu++;
+        BankovniUcet.aktualniCisloUctu ++;
         this.zustatek = zustatek;
         this.cisloUctu = BankovniUcet.aktualniCisloUctu;
         this.klientskeCislo = klientskeCislo;
     }
+
     public int getZustatek() {
         return zustatek;
     }
     //vrátí false v případě neúspěchu
     public boolean vyberPenize(int castka){
-        if(castka > 0 && castka <= this.zustatek){
+        if(castka >0 && castka <= this.zustatek){
             this.zustatek -= castka;
             return true;
         }
         return false;
     }
     public boolean vlozPenize(int castka){
-        if(castka > 0) {
+        if(castka > 0){
             this.zustatek += castka;
             return true;
         }
         return false;
+    }
+
+    @Override
+    public String toString() {
+        return "BankovniUcet{" +
+                "zustatek=" + zustatek +
+                ", cisloUctu=" + cisloUctu +
+                ", klientskeCislo=" + klientskeCislo +
+                '}';
     }
 }
--- src/cz/spsmb/b3i/w10/tridy2/Klient.java
+++ src/cz/spsmb/b3i/w10/tridy2/Klient.java
@@ -1,7 +1,4 @@
 package cz.spsmb.b3i.w10.tridy2;
-//Vytvořte třídu Klient, která bude mít jako jednu členskou
-// proměnnou referenci na objekt třídy BankovniUcet. Modifikujte třídu Banka,
-// aby pracovala s klienty
-// místo účty.
+
 public class Klient {
 }
