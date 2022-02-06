962d9bb1b6670d2869973c8b75a41312207d40e8
ondra <ondrajanosik283@gmail.com>
2022-02-06 19:20:52 +0100

    Dú práce se soubory

--- src/cz/spsmb/b3i/w20/domaci_ukol/Info.java
+++ src/cz/spsmb/b3i/w20/domaci_ukol/Info.java
@@ -1,10 +1,8 @@
 package cz.spsmb.b3i.w20.domaci_ukol;
 
 public interface Info {
-    default void kdoJsem (){
-        System.out.println("[Info] Jsem " + this.getClass().getSimpleName());
-    }
-    static void kdoJsemStat(){
-        System.out.println("[Info] Jsem " + Info.class.getSimpleName());
+    default void KdoJsem() {
+        System.out.println("IDK");
     }
 }
+
