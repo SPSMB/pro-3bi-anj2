4134047890cbc0cf3d14cca2770be712b64d19ff
Lukyys <lukyh735@gmail.com>
2022-04-28 15:27:37 +0200

    +javadoc

--- src/cz/spsmb/b3i/w24/piskorky/PiskorkyStatus.java
+++ src/cz/spsmb/b3i/w24/piskorky/PiskorkyStatus.java
@@ -2,10 +2,22 @@ package cz.spsmb.b3i.w24.piskorky;
 import java.io.Serializable;
 import java.util.*;
 
+/**
+ * Třída reprezentující status Piškvorky
+ */
 public class PiskorkyStatus implements Serializable {
 
+    /**
+     * Jedná se o verzi
+     */
     public final String VERSION = "1.13";
+    /**
+     * n je rozměr hrací plochy a počet vítězných tahů je nastaven na 4
+     */
     public final int nViteznych = 4;
+    /**
+     * Doba mezi koncem tahu prvního hráče a začátkem tahu druhého hráče
+     */
     public final int TIMEOUT = 10000;
     int rozmerHraciPlochy;
     int nTah = 1;
@@ -14,11 +26,13 @@ public class PiskorkyStatus implements Serializable {
     Map[][] herniTlacitka;
     //aktivni Hráč se zde automaticky inicializuje na 0 (LOJZA), netřeba inicializovat
     int aktivniHrac;
-     boolean isStarted;
+    boolean isStarted;
     boolean isEnded;
 
 
-
+    /**
+     * @param rozmerHraciPlochy hraje se na ploše n krát n, kde n je rozměr hrací plochy
+     */
     public PiskorkyStatus(int rozmerHraciPlochy) {
         this.rozmerHraciPlochy = rozmerHraciPlochy;
         //this.herniPlochaHracu = new int[this.rozmerHraciPlochy + 1][this.rozmerHraciPlochy + 1];
@@ -33,6 +47,10 @@ public class PiskorkyStatus implements Serializable {
         this.inicializaceTlacitek();
     }
 
+
+    /**
+     * Resetuje hrací plochu, smaže všechen postup
+     */
     public void clean(){
         this.inicializaceTlacitek();
         this.hraci.clear();
@@ -41,6 +59,10 @@ public class PiskorkyStatus implements Serializable {
         this.isEnded = false;
     }
 
+    /**
+     * @param jmeno -jmeno hrace, pokud již existuje první písmeno v seznamu hráčů, tak se hráč nepřidá
+     * @return
+     */
     public boolean pridatHrace(String jmeno){
         for (String hrac : this.hraci) {
             if(hrac.charAt(0) == jmeno.charAt(0)){
@@ -50,11 +72,19 @@ public class PiskorkyStatus implements Serializable {
         this.hraci.add(jmeno);
         return false;
     }
+
+    /**
+     * Spuštění hry
+     */
      public void start(){
         this.isStarted = true;
 
 
      }
+
+    /**
+     * Pokud hráč udělá svůj tah, tak se přepne na druhého hráče
+     */
      public void prepnutiHrace(){
          if(!this.isEnded){
              //přepnutí hráče
@@ -65,11 +95,12 @@ public class PiskorkyStatus implements Serializable {
          }
 
 
-
      }
 
 
-
+    /**
+     * Aktivování tlačítek pro hru
+     */
     public void inicializaceTlacitek(){
         for (int i = 0; i < this.rozmerHraciPlochy + 1; i++) {
             for (int j = 0; j < this.rozmerHraciPlochy + 1; j++) {
